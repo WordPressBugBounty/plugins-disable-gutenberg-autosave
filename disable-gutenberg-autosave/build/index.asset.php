af84b70a6f2ab7ce16516f096576872ddf7e80fe
Sean Murphy <sean@iamseanmurphy.com>
2024-09-24 12:04:12 +0000

    1.0.12

---
+++ disable-gutenberg-autosave/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '6445ad65c05fcc2cc2e7');
